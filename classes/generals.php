23aea14bd15c5a7f75b7520acf5605fd294dd308
SimoneBenato <sb270298@gmail.com>
2021-10-22 15:47:05 +0200

    scaffholding

---
+++ classes/generals.php
@@ -0,0 +1,64 @@
+<?php
+    class Product {
+        public $name;
+        public $price;
+
+        function __construct($_name, $_price)
+        {
+            $this->name = $_name;
+            $this->price = $_price;
+
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($_name)
+        {
+            $this->name = $_name;
+        }
+
+        function getPrice()
+        {
+            return $this->price;
+        }
+
+        function setPrice($_price)
+        {
+            $this->price = $_price;
+        }
+    };
+
+    class User {
+        public $userName;
+        private $password;
+
+        function __construct($_userName, $_password)
+        {
+            $this->userName = $_userName;
+            $this->password = $_password;
+        }
+
+        function getUserName()
+        {
+            return $this->userName;
+        }
+
+        function setUserName($_userName)
+        {
+            $this->userName = $_userName;
+        }
+
+        function getPassword()
+        {
+            return $this->password;
+        }
+
+        function setPassword($_password)
+        {
+            $this->password = $_password;
+        }
+    }
+?>
